25fda490af4cd38643028f8053c8641bbd8f6dd6
ComedicChimera <forlornsisu@gmail.com>
2018-07-06 00:44:44 -0500

    added missing items from commit

---
+++ whirlwind_org/static/js/index.js
@@ -0,0 +1,54 @@
+let demos = ["concur", "func", "expr", "modules", "control-flow"];
+let demoNum = 1;
+
+const demoCode = [
+    `use include stdio;\nlock $counter = 0;\n\nasync printCounter() {\n\tdo (5) Println(counter);\n}\n\nfunc Main() int {\n\tprintCounter();\n\tdo (5) counter++;\n}`,
+    `func Sum($lst: list[int]) int =>\n\tlst |> ... + |;\n\nfunc Main() int {\n\tSum([1, 2, 3]); // 6\n\tSum([4, 5]); // 9\n}`,
+    `func Sqrt($n: int) {\n\t$t = n;\n\tfor($z = 0; z < 10; z++)\n\t\ty -= (y^2 - n) / (2 * y);\n\treturn y;\n}\n\nfunc Main() int {\n\tSqrt(4); // 2\n\tSqrt(16); // 4\n}`,
+    `\n\nmodule Point2D {\n\tproperty $(x, y): int;\n\n\tfunc property Display() =>
+        "(%d, %d)".Format(this.x, this.y);\n}\n\nfunc Main() {\n\t$p = new Point2D { x=2, y=4 };\n\tp.Display(); // (2, 4)\n}`,
+    `use include stdio;\n\nfunc Main() int {\n\t$input = Scanln();\n\tmatch {\n\t\t? len(input) < 5:
+        \tPrintln("Input is short.");\n\t\t? input.Lower() == "Hello":\n\t\t\tPrintln("Input is a greeting.");\n\t}\n}`
+]
+
+$(() => {
+    $('#demo-left').mousedown(() => {
+        $('#demo-' + demos[0]).removeClass('visible');
+        demos.unshift(demos.pop());
+        $('#demo-' + demos[0]).addClass('visible');
+
+        if (demoNum > 1) demoNum--;
+        else demoNum = 5;
+        $('#demo-example-number').html(demoNum);
+        
+        $('#viewer-content').html(demoCode[demoNum - 1]);
+        highlightEditor();
+    });
+
+    $('#demo-right').mousedown(() => {
+        $('#demo-' + demos[0]).removeClass('visible');
+        demos.push(demos.shift());
+        $('#demo-' + demos[0]).addClass('visible');
+
+        if (demoNum < 5) demoNum++;
+        else demoNum = 1;
+        $('#demo-example-number').html(demoNum);
+        
+        $('#viewer-content').html(demoCode[demoNum - 1]);
+        highlightEditor();
+    });
+
+    // highlight when page ready
+    $('#viewer-content').html(demoCode[0]);
+    highlightEditor();
+});
+
+function htmlUnescape(string) {
+    let doc = new DOMParser().parseFromString(string, "text/html");
+    return doc.documentElement.textContent;
+}
+
+function highlightEditor() {
+    let html = htmlUnescape($('#viewer-content').html());
+    $('#viewer-content').html(Prism.highlight(html, Prism.languages.whirlwind));
+}
---
+++ whirlwind_org/static/js/language-whirlwind.js
@@ -0,0 +1,58 @@
+Prism.languages.whirlwind = {
+    'comment': [
+        {
+            pattern: /\/\*.*\*\//s
+        },
+        {
+            pattern: /\/\/.*/
+        }
+    ],
+    'string': {
+        pattern: /"(?:[^"\\']|\\.)*"/,
+        greedy: true
+    },
+    'char': {
+            pattern: /'(?:[^"\\']|\\.)*'/,
+            greedy: true
+    },
+    'number': [
+        // hex
+        {
+            pattern: /\b0x[0-9A-F]+/
+        },
+        // binary
+        {
+            pattern: /\b0b[10]+/
+        },
+        // numeric
+        {
+            pattern: /\b\d+(\.\d+)?/
+        }
+        
+    ],
+    'constant': /null|this|@[_a-zA-Z]\w*/,
+    'variable': /\$[_a-zA-Z]\w*/,
+    'keyword': [
+        // control flow
+        {
+            pattern: /\b(break|continue|return|yield|for|do|if|elif|else|match|select|case|default|throw|with|except)\b/
+        },
+        // declarations
+        {
+            pattern: /\b(func|async|module|interface|union|constructor)\b/
+        },
+        // data types
+        {
+            pattern: /\b(int|bool|float|char|str|type|array|list|map|byte|long)\b/
+        },
+        // special
+        {
+            pattern: /\b(value|new|use|include|delete)\b/
+        }
+    ],
+    'function': /[a-zA-Z_]\w*(?=\()/,
+    'punctuation':/[{}[\];(),.:]/,
+    'operator': /[+\-*\/%&\^!|><=?\$@]/,
+    'boolean': /true|false/,
+    'modifier': /\b(private|partial|extern|lock|property)\b/,
+};
