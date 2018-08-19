cb8b7b49d40176899c54764b7ae179b62cee9e4c
ComedicChimera <forlornsisu@gmail.com>
2018-08-18 23:50:13 -0500

    renamed Main to main

--- whirlwind_org/static/js/index.js
+++ whirlwind_org/static/js/index.js
@@ -2,12 +2,12 @@ let demos = ["concur", "func", "generic", "modules", "control-flow"];
 let demoNum = 1;
 
 const demoCode = [
-    `use include stdio;\nvolatile $counter = 0;\n\nasync printCounter() {\n\tdo (5) Println(counter);\n}\n\nfunc Main() {\n\tprintCounter();\n\tdo (5) counter++;\n}`,
-    `func Sum($lst: list[int]) int =>\n\tlst |> ... + |;\n\nfunc Main() {\n\tSum([1, 2, 3]); // 6\n\tSum([4, 5]); // 9\n}`,
+    `use include stdio;\nvolatile $counter = 0;\n\nasync printCounter() {\n\tdo (5) Println(counter);\n}\n\nfunc main() {\n\tprintCounter();\n\tdo (5) counter++;\n}`,
+    `func Sum($lst: list[int]) int =>\n\tlst |> ... + |;\n\nfunc main() {\n\tSum([1, 2, 3]); // 6\n\tSum([4, 5]); // 9\n}`,
     `template&lt;T>\nfunc Max($set: list[T], $fn: func($a: T, $b: T)(bool)) \nlist[T]\n{\n\t$mx: T;\n\tfor set|item| => {\n\t\tif (fn(mx, item))\n\t\t\tmx = item;\n\t}\n}`,
     `\n\nmod Point2D {\n\t$(x, y): int;\n\n\tfunc display() str =>
-        "(%d, %d)".format(this.x, this.y);\n}\n\nfunc Main() {\n\t$p = new Point2D { x=2, y=4 };\n\tp.display(); // (2, 4)\n}`,
-    `use include stdio;\n\nfunc Main() {\n\t$input = Scanln();\n\tmatch {\n\t\t? len(input) < 5:
+        "(%d, %d)".format(this.x, this.y);\n}\n\nfunc main() {\n\t$p = new Point2D { x=2, y=4 };\n\tp.display(); // (2, 4)\n}`,
+    `use include stdio;\n\nfunc main() {\n\t$input = Scanln();\n\tmatch {\n\t\t? len(input) < 5:
         \tPrintln("Input is short.");\n\t\t? input.lower() == "Hello":\n\t\t\tPrintln("Input is a greeting.");\n\t}\n}`
 ]
 
