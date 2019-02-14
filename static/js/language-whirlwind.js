4458b5d7c40f11eb9c7f972484ad522e3f0d2784
ComedicChimera <forlornsisu@gmail.com>
2019-02-13 23:33:43 -0600

    worked on home page

---
+++ static/js/language-whirlwind.js
@@ -0,0 +1,70 @@
+Prism.languages.whirlwind = {
+    'comment': [
+        {
+            pattern: /(\/\*[^(\\\*)]*\*\/)/s
+        },
+        {
+            pattern: /\/\/.*/
+        }
+    ],
+    'string': {
+        pattern: /"(?:[^"\\']|\\.)*"/,
+        greedy: true
+    },
+    'char': {
+        pattern: /'(?:[^"\\']|\\.)*'/,
+        greedy: true
+    },
+    'number': [
+        // hex
+        {
+            pattern: /\b0x[0-9A-F]+/
+        },
+        // binary
+        {
+            pattern: /\b0b[10]+/
+        },
+        // numeric
+        {
+            pattern: /\b\d+(\.\d+)?/
+        }
+        
+    ],
+    'variant': {
+        pattern: /\b(variant)\s*(<[\w ,]+>)\s*(\w+)\s*\{/,
+        inside: {
+            'function': /(\w+)\s*(?=\{)/,
+            'keyword': /\b(variant|int|bool|float|char|str|array|list|dict|byte|long|double)\b/,
+            'punctuation': /[{,]/,
+            'operator': /[<>]/
+        }   
+    },
+    'keyword': [
+        // control flow
+        {
+            pattern: /\b(break|continue|return|yield|for|if|elif|else|select|case|default|throw|except|when|after)\b/
+        },
+        // declarations
+        {
+            pattern: /\b(func|async|type|interf|template|struct|constructor|variant|enum|agent)\b/
+        },
+        // variables
+        {
+            pattern: /\b(let|const)\b/
+        },
+        // data types
+        {
+            pattern: /\b(int|bool|float|char|str|array|list|dict|byte|long|double)\b/
+        },
+        // special
+        {
+            pattern: /\b(new|from|include|delete|cast|export|await|ref)\b/
+        }
+    ],
+    'function': /[a-zA-Z_]\w*(?=\()/,
+    'punctuation':/[{}[\];(),.]/,
+    'operator': /[+\-*\/%&\^!|><=?]/,
+    'boolean': /true|false/,
+    'modifier': /\b(priv|vol)\b/,
+    'constant': /\b(this|null|_)\b/,
+};
