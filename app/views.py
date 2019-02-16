9158106099f035feda511f4a6dac3e96ad9a441a
ComedicChimera <forlornsisu@gmail.com>
2019-02-16 00:48:02 -0600

    worked on home page and code examples

--- app/views.py
+++ app/views.py
@@ -4,3 +4,8 @@ from app import app
 @app.route('/')
 def index():
     return app.send_static_file('html/index.html')
+
+
+@app.route('/docs')
+def docs():
+    return app.send_static_file('html/docs.html')
